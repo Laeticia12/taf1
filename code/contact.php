1914a21bd1ca418d6a4906132966ebdcf6b07c66
David Kemo <118942890+David5831@users.noreply.github.com>
2024-03-26 08:44:22 +0100

    Update contact.php

--- code/contact.php
+++ code/contact.php
@@ -1,20 +1,22 @@
 <?php{
 $_servername="localhost";
 $_usename='root';
-$_password='root'
-
-$_bdd->SetAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_exeption)
+$_password='';
+$_dbname='utilisateur';
+try{
+    $con = new PDO("mysql:host=".$_servername.";dbname=;".$_utilisateur,$_username,$_password);
 }
 catch(PDOExeption $e){
-    echo"erreur": "$e->getMessage"
+    die("erreur: ".$e->getMessage());
 }
 
 
-if (isset ($_POST['ok'])) {
+if (isset($_POST['ok'])) {
+    
     $_nom=$_POST['nom'];
     $_prenom=$_POST['prenom'];
     $_email=$_POST['email'];
-    $_message=$_post['message']
+    $_message=$_post['message'];
     # code...
 }
 
